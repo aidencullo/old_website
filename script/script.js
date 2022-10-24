b0760dc15a5ea602ab3f2b4f1f13d2f5abaeeb2f
Aiden Cullo <aidencullo@gmail.com>
2022-10-24 15:55:48 -0500

    absolute mess, trying to create background grid mouse tracker

--- script/script.js
+++ script/script.js
@@ -1,5 +1,13 @@
 function getCursorPosition(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    // Aca pongamos el algoritmo para colocar el ratone y luego brillar el correspondido cuadra
-}
+/*
+    let x = event.clientX
+    let y = event.clientY
+    let rdx = Math.round(x/100)
+    rdx *= 100
+    let rdy = Math.round(x/100)
+    rdy *= 100
+    //alert(rdx);
+*/
+    document.getElementById("square").style.left = x
+    document.getElementById("square").style.top = y
+ }
