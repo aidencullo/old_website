921ad2461e6cc2bfd71af417f3ed1cee41863e87
Aiden Cullo <aidencullo@gmail.com>
2022-10-25 12:22:25 -0500

    gonna leave it like this

--- script/script.js
+++ script/script.js
@@ -1,17 +1,15 @@
 function getCursorPosition(event) {
-
-/*
     let x = event.clientX
     let y = event.clientY
-    let rdx = Math.round(x/100)
+    let rdx = Math.floor(x/100)
     rdx *= 100
-    let rdy = Math.round(x/100)
+    let rdy = Math.floor(y/100)
     rdy *= 100
-    //alert(rdx);
-    document.getElementById("squaree").style.left = x
-    document.getElementById("squaree").style.top = y
-*/
+    document.getElementById("squaree").style.left = rdx
+    document.getElementById("squaree").style.top = rdy
+    document.getElementById("x").textContent = x;
+    document.getElementById("y").textContent = y;
+    document.getElementById("rdx").textContent = rdx;
+    document.getElementById("rdy").textContent = rdy;
 
-    document.getElementById("squaree").style.left = event.clientX;
-    document.getElementById("squaree").style.top = event.clientY;
- }
+}
